ce42dcb2fb3f340908c081d54b558edad764f95a
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2024-01-10 09:16:52 +0100

    Email Service integration

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log("The server started on port 3000 !!!!!!");
+app.listen(3002, () => {
+  console.log("The server started on port 3002 !!!!!!");
 });
 
 app.get("/", (req, res) => {
@@ -21,17 +21,17 @@ app.get("/", (req, res) => {
 
 app.post("/sendmail", (req, res) => {
   console.log("request came");
-  let user = req.body;
-  sendMail(user, info => {
+  let body = req.body;
+  sendMail(body.name, body.email, body.subject, body.msg, info => {
     console.log(`The mail has beed send 😃 and the id is ${info.messageId}`);
     res.send(info);
   });
 });
 
-async function sendMail(user, callback) {
+async function sendMail(name, email, subject, msg, callback) {
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.zoho.eu",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
@@ -41,11 +41,11 @@ async function sendMail(user, callback) {
   });
 
   let mailOptions = {
-    from: '"Fun Of Heuristic"<example.gimail.com>', // sender address
-    to: user.email, // list of receivers
-    subject: "Wellcome to Fun Of Heuristic 👻", // Subject line
-    html: `<h1>Hi ${user.name}</h1><br>
-    <h4>Thanks for joining us</h4>`
+    from: '"mtogo"<noreply@mtogo.live>', // sender address
+    to: email, // list of receivers
+    subject: subject, // Subject line
+    html: `<h1>To ${name}</h1><br>
+    ${msg}`
   };
 
   // send mail with defined transport object
